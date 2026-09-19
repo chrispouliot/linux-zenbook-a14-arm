83e1b98e376b9251de772117b2637f8352bf9021
Chris Pouliot <github@pouliot.me>
2026-09-19 12:12:36 -0700

    Fix syntax error

--- kernel/a14-dsc-blank-test.nix
+++ kernel/a14-dsc-blank-test.nix
@@ -1,5 +1,4 @@
 # Applied after the recorded display stack; disabled unless its boot flag is set.
-  patch --batch --forward --fuzz=0 -p1 < ${../patches/a14-dsc-d3-before-source-test.patch}
 ''
   patch --batch --forward --fuzz=0 -p1 < ${../patches/a14-dsc-defer-sink-clear-test.patch}
   patch --batch --forward --fuzz=0 -p1 < ${../patches/a14-dsc-d3-before-source-test.patch}
